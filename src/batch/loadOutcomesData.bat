e193480cfed91bd637f3035376adb397a849450b
AdamEdgar <adam.ofwat@gmail.com>
2021-07-21 17:48:47 +0100

    Changed so that loadOutcomesData.bat will run Ken's files and then Andy's all in one go

--- src/batch/loadOutcomesData.bat
+++ src/batch/loadOutcomesData.bat
@@ -7,9 +7,8 @@ echo %date% %time% running loadOutcomesData.bat ...
 
 @REM SET DBT_TARGET=dev
 SET DBT_TARGET=power_bi
-SET UPDATE_AUTHORISED_BY="andy.duff"
-@REM SET UPDATE_AUTHORISED_BY="ken.macdonald"
 SET DBT_DIR=\dbt\demo_dbt
+SET UPDATE_AUTHORISED_BY="ken.macdonald"
 
 echo %date% %time% Ensuring we are in the batch directory.
 if NOT %CD%\ == %~dp0 (
@@ -18,15 +17,21 @@ if NOT %CD%\ == %~dp0 (
     cd %~dp0
 )
 
-@REM echo %date% %time% calling .\loadPR14base.bat with DBT_TARGET = %DBT_TARGET%
-@REM call .\loadPR14base.bat
+echo %date% %time% calling .\loadPR14base.bat with DBT_TARGET = %DBT_TARGET%
+call .\loadPR14base.bat
 
-@REM echo %date% %time% calling .\loadPR14submeasures.bat with DBT_TARGET = %DBT_TARGET%
-@REM call .\loadPR14submeasures.bat
+echo %date% %time% calling .\loadPR14submeasures.bat with DBT_TARGET = %DBT_TARGET%
+call .\loadPR14submeasures.bat
 
-@REM echo %date% %time% calling .\loadPR19base.bat with DBT_TARGET = %DBT_TARGET%
-@REM call .\loadPR19base.bat
+echo %date% %time% calling .\loadPR19base.bat with DBT_TARGET = %DBT_TARGET%
+call .\loadPR19base.bat
 
+copy C:\dev\projects\OceanPython\src\main\resources\updates\KMc\* C:\dev\projects\OceanPython\src\main\resources\updates
+echo %date% %time% calling .\loadPR19updates.bat with DBT_TARGET = %DBT_TARGET%
+call .\loadPR19updates.bat
+
+SET UPDATE_AUTHORISED_BY="andy.duff"
+copy C:\dev\projects\OceanPython\src\main\resources\updates\AD\* C:\dev\projects\OceanPython\src\main\resources\updates
 echo %date% %time% calling .\loadPR19updates.bat with DBT_TARGET = %DBT_TARGET%
 call .\loadPR19updates.bat
 
