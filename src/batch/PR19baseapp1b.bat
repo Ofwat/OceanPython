ff8a3519447901cb10f75df7bd3629e6b8b0e0d7
Niyati.Wawre <niyati.wawre@ofwat.gov.uk>
2021-07-26 16:59:17 +0000

    script app1b

---
+++ src/batch/PR19baseapp1b.bat
@@ -0,0 +1,37 @@
+@echo off
+
+echo .
+echo  %date% %time% calling loadPR19baseapp1b.bat ...
+echo  %date% %time% Ensuring we are in the batch directory.
+if NOT %CD%\ == %~dp0 (
+    echo  %date% %time% Changing to dir: %~dp0
+    %~d0
+    cd %~dp0
+)
+
+echo  %date% %time% Changing to the main python dir.
+cd ..\main\python
+
+echo  %date% %time% Using the dbt power bi settings.
+python.exe .\tools\replaceDBTTarget.py --from_target dev --to_target %DBT_TARGET%
+
+echo  %date% %time% Loading data from Excel to DB.
+echo  %date% %time% running ... python.exe .\outcomesDataProc.py PR19baseapp1b --user %UPDATE_AUTHORISED_BY%  --comment "Initial load" --input_file "..\resources\Pauls_latest_PR19.xlsx"
+python.exe .\outcomesDataProc.py PR19baseapp1b --user %UPDATE_AUTHORISED_BY%  --comment "Initial load" --input_file "..\resources\Pauls_latest_PR19.xlsx"
+
+echo  %date% %time% run dbt
+cd %DBT_DIR%
+dbt run
+
+echo  %date% %time% Changing back to the batch dir
+cd %~dp0
+echo  %date% %time% Changing to the main python dir.
+cd ..\main\python
+
+echo  %date% %time% Switching back to the dbt dev settings
+python.exe .\tools\replaceDBTTarget.py --from_target %DBT_TARGET% --to_target dev
+
+echo  %date% %time% Changing back to the batch dir
+cd %~dp0
+
+echo  %date% %time% completed loadPR19baseapp1b.bat
